462e9930acbff220e13284e286071210bd56c2bd
Allahdad Muhammadi <allahdadmuhammadi01@gmail.com>
2026-08-30 16:25:37 +0430

    Update book ticket modal to prevent closing on backdrop click
    - Modified the `book_ticket_modal.php` to include `data-backdrop="static"` attribute, ensuring the modal remains open when clicking outside of it, enhancing user experience during ticket booking.

--- modals/ticket/book_ticket_modal.php
+++ modals/ticket/book_ticket_modal.php
@@ -1,5 +1,5 @@
     <!-- Book Ticket Modal -->
-    <div class="modal fade" id="bookTicketModal" tabindex="-1" role="dialog">
+    <div class="modal fade" id="bookTicketModal" tabindex="-1" role="dialog" data-backdrop="static">
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
